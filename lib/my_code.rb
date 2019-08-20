d4b3a6b01c5099b67459e13e8a2c477126394338
Brett Saxon <brettcsaxon@gmail.com>
2019-08-20 17:54:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,19 @@ def map(source_array)
   end
   source_array
 end
+
+
+def reduce(array, starting_point = nil)
+  if starting_point
+    value = starting_point
+    i = 0
+  else
+    value = array[0]
+    i = 1
+  end
+  while i < array.length
+    value = yield(value, array[i])
+    i += 1
+  end
+  value
+end
